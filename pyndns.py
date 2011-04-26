51caa9f5eefeb22d4ec3582f620449679b5032b7
Christian Blades <christian@blades-server.(none)>
2011-04-25 22:26:36 -0400

    First commit.
    Method to grab current IP from dyndns.

---
+++ pyndns.py
@@ -0,0 +1,18 @@
+"""
+PynDns - A Python DynDns client
+Written By: Christian Blades (christian DOT blades AT docblades DOT com)
+April 25 2011
+"""
+
+import urllib2, re
+from BeautifulSoup import BeautifulSoup
+
+
+def get_ip_from_dyndns():
+    url = "http://checkip.dyndns.com"
+    response = urllib2.urlopen(url)
+    page = BeautifulSoup(response)
+    ip_match = re.match("Current IP Address: (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})", page.body.text)
+    ip_addr = ip_match.group(1)
+    return ip_addr
+
