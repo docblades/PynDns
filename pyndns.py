4453a7afb226f1d493344878deef05557adeddf3
Christian Blades <christian@blades-server.(none)>
2011-04-26 23:37:54 -0400

    More tests. Moved data persistance object into the data module

--- pyndns.py
+++ pyndns.py
@@ -15,38 +15,5 @@ def get_ip_from_dyndns():
     ip_addr = ip_match.group(1)
     return ip_addr
 
-class Config(object):
-    hostname = None
-    username = None
-    password = None
-
-    def to_dict(self):
-        me_dict = {"hostname": self.hostname,
-                   "username": self.username,
-                   "password": self.password}
-        return me_dict
-
-    def dump_to_json(self):
-        return json.dumps(self.to_dict())
-
-    def from_dict(self, config_dict):
-        self.hostname = config_dict["hostname"]
-        self.username = config_dict["username"]
-        self.password = config_dict["password"]
-
-    def from_json(self, json_string):
-        me_dict = json.loads(json_string)
-        self.from_dict(me_dict)
-
-    def from_file(self, config_file):
-        me_dict = json.load(config_file)
-        self.from_dict(me_dict)
-
-    def __str__(self):
-        return self.username + "@" + self.hostname
-
-    def __repr__(self):
-        return "<pydndns.Config object hostname:" + self.hostname + " username: " + self.username + ">"
-    
     
         
--- tests/pyndns_test.py
+++ tests/pyndns_test.py
@@ -1,10 +1,11 @@
-import unittest
-from pyndns import pyndns
+import unittest2
+from pyndns import data
+from StringIO import StringIO
 
-class ConfigJSONTests(unittest.TestCase):
+class ConfigJSONTests(unittest2.TestCase):
 
     def setUp(self):
-        self.config = pyndns.Config()
+        self.config = data.Config()
 
     def tearDown(self):
         self.config = None
@@ -21,4 +22,39 @@ class ConfigToDict(ConfigJSONTests):
         self.assertEqual("testuser", result["username"])
         self.assertEqual("testpass", result["password"])
 
-    
+class ConfigToJSON(ConfigJSONTests):
+    def runTest(self):
+        self.config.hostname = "testhost2"
+        self.config.username = "testuser2"
+        self.config.password = "testpass2"
+
+        result = self.config.dump_to_json()
+
+        self.assertIsNotNone(result)
+        self.assertRegexpMatches(result, "\"username\"\s?:\s?\"testuser2\"")
+        self.assertRegexpMatches(result, "\"hostname\"\s?:\s?\"testhost2\"")
+        self.assertRegexpMatches(result, "\"password\"\s?:\s?\"testpass2\"")
+
+class ConfigFromJSON(ConfigJSONTests):
+    def runTest(self):
+        testJSON = """{"hostname": "testhost3", "username": "testuser3", "password": "testpass3"}"""
+        
+        self.config.from_json(testJSON)
+
+        self.assertEqual("testhost3", self.config.hostname)
+        self.assertEqual("testuser3", self.config.username)
+        self.assertEqual("testpass3", self.config.password)
+
+class ConfigFromFile(ConfigJSONTests):
+    def runTest(self):
+        testJSON = """{"hostname": "testhost4", "username": "testuser4", "password": "testpass4"}"""
+
+        fakeFile = StringIO(testJSON)
+
+        self.config.from_file(fakeFile)
+
+        self.assertEqual("testhost4", self.config.hostname)
+        self.assertEqual("testuser4", self.config.username)
+        self.assertEqual("testpass4", self.config.password)
+
+
