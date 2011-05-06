7a94576737fd8158c38acc4d950aeb4ff101573f
Christian Blades <christian@blades-server.(none)>
2011-05-05 21:02:06 -0400

    Chose License, writing setup.py, moved __main__.py into package tree

---
+++ pyndns/__main__.py
@@ -0,0 +1,1 @@
+pyndns.py
---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(name='Pyndns',
+      version='0.1.0',
+      description='A Python DynDns update client',
+      author='Christian Blades',
+      author_email='christian.blades@docblades.com',
+      url='http://docblades.com',
+      packages=['pyndns'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+        'Natural Language :: English',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Programming Language :: Python',
+        'Operating System :: OS Independent',
+        
+        ]
+      )
